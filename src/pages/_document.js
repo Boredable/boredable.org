ca6b772f501364852948c7c9a653d4eba2b03f13
Tyler Rilling <tyler@underlost.net>
2024-11-19 20:35:25 -0800

    Add GA code

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <GoogleAnalytics gaId="G-L8HH1QQNH6" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
